ba422fadc32341b736bae7d8c66989388a6698ec
Andrey <zhegalovan@mail.ru>
2021-05-17 12:52:53 +0300

    minor fix

--- game-session-service/src/main/java/zhegalov/course/work/Main.java
+++ game-session-service/src/main/java/zhegalov/course/work/Main.java
@@ -2,9 +2,7 @@ package zhegalov.course.work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "zhegalov.course.work.feign")
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
