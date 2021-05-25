08aa47122cd787b86ce15f64acaec023158cf999
Andrey <zhegalovan@mail.ru>
2021-05-25 08:49:06 +0300

    updated secirity config

--- game-session-service/src/main/java/zhegalov/course/work/config/SecurityConfig.java
+++ game-session-service/src/main/java/zhegalov/course/work/config/SecurityConfig.java
@@ -3,9 +3,22 @@ package zhegalov.course.work.config;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.web.SecurityFilterChain;
+
+import feign.RequestInterceptor;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -23,5 +36,39 @@ public class SecurityConfig {
             .oauth2Client(withDefaults())
             .csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+
+        return requestTemplate -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId("expression-client-oidc")
+                                                                            .principal(authentication)
+                                                                            .build();
+            OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+            OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+
+            requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getTokenValue());
+        };
+    }
+    @Bean
+    OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        // @formatter:off
+        final var authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
+                        .clientCredentials()
+                        .build();
+        // @formatter:on
+        final var authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
     }
 }
