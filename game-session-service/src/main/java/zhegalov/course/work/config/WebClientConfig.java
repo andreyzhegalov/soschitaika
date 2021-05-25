15ff6042c248e488788eeb8687c404d534ec76c1
Andrey <zhegalovan@mail.ru>
2021-05-25 08:45:34 +0300

    repared feign client

--- game-session-service/src/main/java/zhegalov/course/work/config/WebClientConfig.java
+++ game-session-service/src/main/java/zhegalov/course/work/config/WebClientConfig.java
@@ -2,16 +2,38 @@ package zhegalov.course.work.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import feign.RequestInterceptor;
 
 @Configuration
 public class WebClientConfig {
+    @Bean
+    public RequestInterceptor requestInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+
+        return requestTemplate -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId("expression-client-oidc")
+                                                                            .principal(authentication)
+                                                                            .build();
+            OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+            OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+
+            requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getTokenValue());
+        };
+    }
 
     // @formatter:off
     @Bean
